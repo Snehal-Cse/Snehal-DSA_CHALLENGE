f462f9a6105a23dd64c3085c4d28c4c8c500f14e
Snehal <117597375+Snehal-Cse@users.noreply.github.com>
2024-07-25 21:47:28 +0530

    Time: 2 ms (39.81%) | Memory: 9.6 MB (13.18%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
-    int f(vector<int>&nums, vector<int>&dp, int n){
-     if(n==0){
-         return nums[0];
-     }   
-   if(n<0){
-            return 0;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-     int left = nums[n]+ f(nums, dp, n-2);
-      int right = 0 + f(nums, dp, n-1);
-        return dp[n]= max(left, right); 
-        
-    }
+    
+    
     int rob(vector<int>& nums) {
-        int n = nums.size();
-      vector<int>dp(n, -1);
-       return f(nums, dp, n-1);
+          int n = nums.size();
+         if (n == 0) return 0; // Edge case: empty array
+        if (n == 1) return nums[0]; // Edge case: only one house
+      
+       vector<int>dp(n, 0);
+       dp[0]=nums[0];
+        dp[1]= max(nums[0], nums[1]);
+      for(int i=2;i<n;i++){
+     
+        
+            int  left = nums[i]+ dp[i-2] ;
+          
+        
+         int right = 0 + dp[i-1];
+         dp[i]= max(left, right); 
+      }
+        
+        
+        return dp[n-1];
+        
+        
+        
+        
+        
+        
+        
+        
     }
 };

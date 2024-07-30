8927c24cb4a6f10e6f8a210c5384765faab8c6fb
Snehal <117597375+Snehal-Cse@users.noreply.github.com>
2024-07-30 20:17:08 +0530

    Time: 242 ms (18.42%) | Memory: 105.7 MB (23.05%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/ones-and-zeroes.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    int f(vector<string>& strs, vector<vector<vector<int>>>&dp, int i, int m, int n){
-        if(i<0){
-            return 0;
-        }
-        if(dp[i][m][n]!=-1){
-            return dp[i][m][n];
-        }
-        int cntZero = count(strs[i].begin(), strs[i].end(), '0');
-        int cntOne = strs[i].size()-cntZero;
-        int take =0;
-        if(m-cntZero>=0 && n-cntOne>=0){
-       take =1+f(strs, dp, i-1, m-cntZero, n-cntOne);
-        }
-        int notTake = f(strs, dp, i-1, m, n);
-        return dp[i][m][n]=max(take, notTake);
-    }
     
     int findMaxForm(vector<string>& strs, int m, int n) {
         
-    vector<vector<vector<int>>>dp(strs.size(), vector<vector<int>>(m+1, vector<int>(n+1, -1)));
-    return f(strs, dp, strs.size()-1, m, n);    
-        
+    vector<vector<vector<int>>>dp(strs.size()+1, vector<vector<int>>(m+1, vector<int>(n+1, 0)));
+    for(int i=1;i<=strs.size();i++){
+        for(int j=0;j<=m;j++){
+            for(int k =0;k<=n;k++){
+               int cntZero = count(strs[i-1].begin(), strs[i-1].end(), '0');
+                int cntOne = strs[i-1].size()-cntZero; 
+                int take =0;
+                if(j-cntZero>=0 && k-cntOne>=0){
+               take =1+dp[i-1][ j-cntZero][k-cntOne] ;
+        }
+              int notTake = dp[i-1][j][k];
+              dp[i][j][k]= max(take, notTake);
+            }
+        }
+    }
+       return dp[strs.size()][m][n]; 
     }
 };

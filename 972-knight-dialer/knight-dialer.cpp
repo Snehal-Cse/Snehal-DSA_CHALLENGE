ce5550e752576c190333cc96834be6db5dba6333
Snehal <117597375+Snehal-Cse@users.noreply.github.com>
2024-08-18 22:29:22 +0530

    Time: 92 ms (54.13%) | Memory: 39.7 MB (32.00%) - LeetSync

--- 972-knight-dialer/knight-dialer.cpp
+++ 972-knight-dialer/knight-dialer.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
-    int MOD = 1000000007;
-    vector<vector<int>>moves = {
+  vector<vector<int>>arr = {
         {4,6},
-        {6,8},
-        {7,9},
-        {4,8},
-        {3,9,0},
+        {6, 8},
+        {7, 9},
+        {8, 4},
+        {0, 3, 9},
         {},
-        {1,7,0},
-        {2,6},
+        {1, 7, 0},
+        {2, 6},
         {1,3},
-        {4,2},   
+        {2,4}  
     };
-    long f(vector<vector<int>>&dp, int remain, int dig){
-        if(remain ==0){
+    int f(vector<vector<int>>&dp, int remain, int dig){
+        if(remain == 0){
             return 1;
         }
         if(dp[remain][dig]!=-1){
             return dp[remain][dig];
         }
-        long ways = 0;
-        for(auto d : moves[dig]){
-            ways += f(dp, remain-1, d);
-            ways = ways%MOD;
-        }
-        
-        return dp[remain][dig]= ways;
-        
+        int ways =0;
+        for(auto d : arr[dig]){
+          ways += f(dp, remain-1, d);
+          ways = ways%1000000007;
+        }  
+        return dp[remain][dig] = ways;
     }
     int knightDialer(int n) {
      vector<vector<int>>dp(n, vector<int>(10, -1));
-        int ans=0;
-        for(int i=0;i<10;i++){
-           ans += f(dp, n-1, i);
-            ans = ans%MOD;
-        }
-        return ans;
+     int ans =0;
+     for(int i=0;i<10;i++){
+         ans += f(dp, n-1, i);
+         ans = ans%1000000007;
+     }
+     return ans;
     }
 };

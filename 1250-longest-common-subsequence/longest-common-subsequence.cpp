7470298a58664becdf6e9e5bc2661d2ab82b6376
Snehal <117597375+Snehal-Cse@users.noreply.github.com>
2024-08-22 08:43:13 +0530

    Time: 23 ms (65.93%) | Memory: 25.8 MB (54.20%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -5,22 +5,17 @@ public:
       int n1 = text1.size();
       int n2 = text2.size();
       vector<vector<int>>dp(n1+1, vector<int>(n2+1, 0));
-        for(int i=0;i<=n1;i++){
-            dp[i][0] =0;
-        }
-        for(int i=0;i<=n2;i++){
-            dp[0][i] =0;
-        }
-          int cnt = 0;
+       
+      
       for(int i=1;i<=n1;i++){
           for(int j=1;j<=n2;j++){ 
             if(text1[i-1]==text2[j-1]){            
-                    cnt = 1 + dp[i-1][j-1];
+                    dp[i][j] = 1 + dp[i-1][j-1];
            }
         else{
-            cnt = max(0+ dp[i-1][j], 0+ dp[i][j-1]);
+            dp[i][j] = max(0+ dp[i-1][j], 0+ dp[i][j-1]);
         }
-              dp[i][j]= cnt;
+             
           }
       } 
         return dp[n1][n2];

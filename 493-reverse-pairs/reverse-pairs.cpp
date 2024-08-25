98dc657294c15f76afc872ce062d5f06d1b325c6
Snehal <117597375+Snehal-Cse@users.noreply.github.com>
2024-08-26 05:22:02 +0530

    Time: 257 ms (79.57%) | Memory: 136.5 MB (71.99%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.cpp
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -1,53 +1,59 @@
 class Solution {
 public:
-    int cnt =0;
-    void merge(vector<int>&nums, int s, int e, int m){
-         int i = s;
-         int j = m+1;
-         vector<int>temp;
-         while(i<=m && j<=e){
-             if(nums[i]<=nums[j]){
-                 temp.push_back(nums[i]);
-                 i++;
-             }
-             else{
-                 temp.push_back(nums[j]);
-                 j++;
-             }
-         }
-        while(i<=m){
-            temp.push_back(nums[i]);
-            i++;
-        }
-        while(j<=e){
-            temp.push_back(nums[j]);
+   
+long long cnt=0;
+void merge(vector<int>&arr, int s, int m, int e){
+      vector<long long>a(e-s+1);
+      int l =s;
+      int r = m+1;
+      int k =0;
+      while(l<=m && r<=e){
+          if(arr[l]<arr[r]){
+              a[k++] = arr[l++];
+
+          }
+          else{
+              a[k++] = arr[r++];  
+          }
+      }
+      while(l<=m){
+          a[k++]=arr[l++];
+      }
+      while(r<=e){
+          a[k++]=arr[r++];
+      }
+     for(int k=0;k<a.size();k++){
+         arr[s+k]= a[k];
+     } 
+}
+void countPair(vector<int>&arr, int s,int m, int e){
+     int j = m+1;
+    for(int i=s; i<=m;i++){
+        while(j<=e && arr[i]>2LL*arr[j]){
             j++;
         }
-        for(int i=0;i<temp.size();i++){
-            nums[s+i]= temp[i];
-        }
-        
+        cnt+=(j-(m+1));
     }
-    void countPairs(vector<int>&nums, int s, int e, int m){
-         int right = m+1;
-         for(int i=s;i<=m;i++){
-             while(right<=e && nums[i]> 2LL*nums[right]){
-                 right++;
-             }
-           cnt += (right - (m+1));
-         }
+    
+}  
+    
+
+void mergeSort(vector<int>&arr, int s, int e){
+    if(s<e){
+        int m = s + (e-s)/2;
+        mergeSort(arr, s, m);
+        mergeSort(arr, m+1, e);
+        countPair(arr, s, m, e);
+        merge(arr, s, m, e);
     }
-    void mergeSort(vector<int>&nums, int s, int e){
-        if(s<e){
-            int m = s+(e-s)/2;
-            mergeSort(nums, s, m);
-            mergeSort(nums, m+1, e);
-            countPairs(nums,s,e,m);
-            merge(nums, s, e, m);
-        }
+}
+  
+    int reversePairs(vector<int>& nums){
+      int s = 0;
+        int n = nums.size();
+      int e = n-1;
+      mergeSort(nums, s, e);
+      return cnt;
     }
-    int reversePairs(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size()-1);   
-        return cnt;    
-    }
+
 };

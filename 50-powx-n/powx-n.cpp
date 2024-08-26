3fd3bd16a6a8711648aaa4eedc20238c5ff7d61c
Snehal <117597375+Snehal-Cse@users.noreply.github.com>
2024-08-26 05:33:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (34.79%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,7 +1,25 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-       return pow(x,n); 
+        double ans=1.0;
+     long long nCopy = n;
+        if(nCopy<0){
+            nCopy*=-1;
+        }
+        while(nCopy>0){
+            if(nCopy%2){
+               ans = ans*x;
+                nCopy = nCopy-1;   
+            }
+            else{
+                x = x*x;
+                nCopy/=2; 
+            }   
+        }
+        if(n<0){
+            ans = double(1.0)/double(ans);
+        }
+        return ans;
         
     }
 };
